83e9d4d66d765437cd6d07bf283b134bfd3aa1ea
Eric L. Hepperle <codewizard13@gmail.com>
2024-07-29 07:28:01 -0500

    ADD discover.php:  About us page for testing menu

---
+++ discover.php
@@ -0,0 +1,36 @@
+<?php
+  include_once 'header.php';
+?>
+
+    <section class="index-intro">
+      <h1>About Us</h1>
+      <p>This is the about / discover page.</p>
+    </section>
+
+    <section class="index-categories">
+      <h2>Some Basic Categories</h2>
+
+      <!-- .index-categories-list -->
+      <div class="index-categories-list">
+
+        <div class="cat-box">
+          <h3>Fun Stuff</h3>
+        </div>
+        <div class="cat-box">
+          <h3>Serious Stuff</h3>
+        </div>
+        <div class="cat-box">
+          <h3>Exciting Stuff</h3>
+        </div>
+        <div class="cat-box">
+          <h3>Boring Stuff</h3>
+        </div>
+
+
+      </div>
+      <!-- // .index-categories-list -->
+
+    </section>
+
+<?php
+include_once 'footer.php';
