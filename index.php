6d0df357257b1c2b0e2ed200b4448f39fc871f49
Eric L. Hepperle <codewizard13@gmail.com>
2024-07-29 04:33:11 -0500

    UPDATE index.php: MATCH to his

--- index.php
+++ index.php
@@ -36,8 +36,43 @@ include_once 'includes/dbh.inc.php';
   </div>
 </nav>
 
+<!-- .wrapper -->
+<div class="wrapper">
 
+  <section class="index-intro">
+    <h1>This is an Introduction</h1>
+    <p>Here is an important paragraph that explains the purpose of the website.</p>
+  </section>
+
+  <section class="index-categories">
+    <h2>Some Basic Categories</h2>
+
+    <!-- .index-categories-list -->
+    <div class="index-categories-list">
+
+      <div class="cat-box">
+        <h3>Fun Stuff</h3>
+      </div>
+      <div class="cat-box">
+        <h3>Serious Stuff</h3>
+      </div>
+      <div class="cat-box">
+        <h3>Exciting Stuff</h3>
+      </div>
+      <div class="cat-box">
+        <h3>Boring Stuff</h3>
+      </div>
+
+
+    </div>
+    <!-- // .index-categories-list -->
+
+  </section>
+
+</div>
+<!-- // .wrapper -->
 
 </body>
-
 </html>
+
+<script src="js/script.js"></script>
