970c0b98566de2dd8db1c8309c43637866395068
Eric L. Hepperle <codewizard13@gmail.com>
2024-07-30 13:42:08 -0500

    ADD blog.php

---
+++ blog.php
@@ -0,0 +1,36 @@
+<?php
+  include_once 'header.php';
+?>
+
+    <section class="index-intro">
+      <h1>Blog</h1>
+      <p>This is where the blog posts are.</p>
+    </section>
+
+    <section class="index-categories">
+      <h2>Some Basic Categories</h2>
+
+      <!-- .index-categories-list -->
+      <div class="index-categories-list">
+
+        <div class="blog-post">
+          <h3>Fun Stuff</h3>
+        </div>
+        <div class="blog-post">
+          <h3>Serious Stuff</h3>
+        </div>
+        <div class="blog-post">
+          <h3>Exciting Stuff</h3>
+        </div>
+        <div class="blog-post">
+          <h3>Boring Stuff</h3>
+        </div>
+
+
+      </div>
+      <!-- // .index-categories-list -->
+
+    </section>
+
+<?php
+include_once 'footer.php';
